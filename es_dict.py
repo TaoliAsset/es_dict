9c3c035d2b045f692f52bad887ea0f3829512990
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-09-23 10:48:02 +0800

    es_dict

--- es_dict.py
+++ es_dict.py
@@ -1,12 +1,26 @@
-class js_dict(dict):
+class es_dict(dict):
     def __init__(self, *arg, **kwarg):
         super().__init__(*arg, **kwarg)
 
     def __getattr__(self, name):
+
         if name in self:
-            return self[name]
+            value = self[name]
+
+            if isinstance(value, dict):
+                return es_dict(value)
+            else:
+                return self[name]
+
         else:
             return None
 
     def __setattr__(self, name: str, value) -> None:
         self[name] = value
+
+
+if __name__ == "__main__":
+    a = {"c": {"d": 1}}
+    b = es_dict(a)
+    print(b, a)
+    print(b.c.d == a["c"])
