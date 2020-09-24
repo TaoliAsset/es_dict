96f1ba7c883afd4462add2c9761c404f7b3bdd1d
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-09-24 11:38:54 +0800

    trans_to_es

--- es_py.py
+++ es_py.py
@@ -2,73 +2,58 @@
 class es_dict(dict):
     def __init__(self, *arg, **kwarg):
         super().__init__(*arg, **kwarg)
+        for key in self:
+            self[key] = trans_to_es(self[key])
 
     def __getattr__(self, name):
+        return super().get(name)
 
-        if name in self:
-            value = self[name]
+    def __setattr__(self, name, value):
+        self[name] = trans_to_es(value)
 
-            if isinstance(value, dict) and not isinstance(value, es_dict):
-                self[name] = es_dict(value)
-                value = self[name]
-            elif isinstance(value, list) and not isinstance(value, es_list):
-                self[name] = es_list(value)
-                value = self[name]
-
-            return value
-
-        else:
-            return None
-
-    def __setattr__(self, name, value) -> None:
-        # print(" __setitem__ ", " self ", self,
-        #       " name ", name, " value ", value, " type ", type(value))
-        if isinstance(value, dict) and not isinstance(value, es_dict):
-            self[name] = es_dict(value)
-        elif isinstance(value, list) and not isinstance(value, es_list):
-            self[name] = es_list(value)
-        else:
-            self[name] = value
+    def update(self, value):
+        super().update(trans_to_es(value))
 
 
 class es_list(list):
     def __init__(self, *arg, **kwarg):
         super().__init__(*arg, **kwarg)
+        for i in range(self.length):
+            self[i] = trans_to_es(self[i])
+            pass
 
     @property
     def length(self):
         return len(self)
 
     def __getitem__(self, name):
-        if isinstance(name, int):
-            if name < self.length and name >= - self.length:
-                value = super().__getitem__(name)
-
-                if isinstance(value, dict) and not isinstance(value, es_dict):
-                    self[name] = es_dict(value)
-                    value = self[name]
-                if isinstance(value, list) and not isinstance(value, es_list):
-                    self[name] = es_list(value)
-                    value = self[name]
-
-                return value
-            else:
-                return None
-        else:
+        if name < self.length and name >= - self.length:
             value = super().__getitem__(name)
             return value
+        else:
+            return None
 
     def __setitem__(self, name, value) -> None:
-        # print(" __setitem__ ", " self ", self,
-        #       " name ", name, " value ", value, " type ", type(value))
-        if isinstance(value, dict) and not isinstance(value, es_dict):
-            super().__setitem__(name,  es_dict(value))
-        elif isinstance(value, list) and not isinstance(value, es_list):
-            super().__setitem__(name, es_list(value))
-        else:
-            super().__setitem__(name, value)
+        super().__setitem__(name, trans_to_es(value))
 
-    def __iter__(self):
-        for i in range(self.length):
-            self[i]
-        return super().__iter__()
+    def append(value):
+        super().append(trans_to_es(value))
+
+    def extend(value):
+        super().extend(trans_to_es(value))
+
+    def insert(index, value):
+        super().insert(index, trans_to_es(value))
+
+
+def trans_to_es(value):
+    if isinstance(value, dict) and not isinstance(value, es_dict):
+        for key in value:
+            value[key] = trans_to_es(value[key])
+        return es_dict(value)
+    elif isinstance(value, list) and not isinstance(value, es_list):
+        for i in range(len(value)):
+            value[i] = trans_to_es(value[i])
+        return es_list(value)
+    else:
+        return value
